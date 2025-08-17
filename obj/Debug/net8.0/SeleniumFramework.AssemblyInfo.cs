d3ac3dfe40c7030e4e9d189037dc598d6cc89def
anuroo <anuroop72@gmail.com>
2025-08-17 22:33:30 +0530

    Latest August17th

--- obj/Debug/net8.0/SeleniumFramework.AssemblyInfo.cs
+++ obj/Debug/net8.0/SeleniumFramework.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SeleniumFramework")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61c09b2f61238d78290f479fed46f2e01432e597")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c780fe815153cf36e0dcb8996004eb6ff8ba1a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("SeleniumFramework")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SeleniumFramework")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
